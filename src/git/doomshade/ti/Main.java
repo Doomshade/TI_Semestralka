7b7090b9f0471dcc6d877342d09d05b7a84a53e4
Jakub Šmrha <kuba.smrha@gmail.com>
2021-01-16 20:23:13 +0100

    init

--- src/git/doomshade/ti/Main.java
+++ src/git/doomshade/ti/Main.java
@@ -1,7 +1,23 @@
 package git.doomshade.ti;
 
-public class Main {
-    public static void main(String[] args) {
+import javax.swing.*;
+import java.util.Scanner;
 
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        MainFrame frame = new MainFrame("TI");
+
+        frame.init();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    private static int validateInput(String s) {
+        if (s.equalsIgnoreCase("exit")) return -1;
+
+        return 1;
     }
 }
